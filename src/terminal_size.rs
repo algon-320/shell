bc19db49429f442ed6f650997fc52302b683eb0f
algon-320 <algon.0320@gmail.com>
2022-11-05 00:11:18 +0900

    Refactor terminal_size module

--- src/terminal_size.rs
+++ src/terminal_size.rs
@@ -1,7 +1,5 @@
 #![allow(unused)]
 
-nix::ioctl_read_bad!(tiocgwinsz, nix::libc::TIOCGWINSZ, nix::pty::Winsize);
-
 use std::sync::atomic::{AtomicU16, Ordering};
 static ROWS: AtomicU16 = AtomicU16::new(0);
 static COLS: AtomicU16 = AtomicU16::new(0);
@@ -14,22 +12,6 @@ pub fn get_cols() -> u16 {
     COLS.load(Ordering::SeqCst)
 }
 
-pub fn init() {
-    update();
-
-    use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
-    let action = SigAction::new(
-        SigHandler::Handler(sigwinch_handler),
-        SaFlags::empty(),
-        SigSet::empty(),
-    );
-    unsafe { sigaction(Signal::SIGWINCH, &action).expect("sigaction for SIGWINCH") };
-}
-
-extern "C" fn sigwinch_handler(_: i32) {
-    update();
-}
-
 pub fn update() {
     let mut winsize = nix::pty::Winsize {
         ws_row: 0,
@@ -37,7 +19,23 @@ pub fn update() {
         ws_xpixel: 0,
         ws_ypixel: 0,
     };
+
+    nix::ioctl_read_bad!(tiocgwinsz, nix::libc::TIOCGWINSZ, nix::pty::Winsize);
     unsafe { tiocgwinsz(0, &mut winsize as *mut nix::pty::Winsize) }.expect("ioctl");
+
     ROWS.store(winsize.ws_row, Ordering::SeqCst);
     COLS.store(winsize.ws_col, Ordering::SeqCst);
+}
+
+pub fn init() {
+    update();
+
+    use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
+    let handler = SigHandler::Handler(sigwinch_handler);
+    let action = SigAction::new(handler, SaFlags::empty(), SigSet::empty());
+    unsafe { sigaction(Signal::SIGWINCH, &action).expect("sigaction for SIGWINCH") };
+}
+
+extern "C" fn sigwinch_handler(_: i32) {
+    update();
 }
